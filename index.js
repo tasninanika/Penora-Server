4da0d9789c870208b231a9abee3ad3c6d69a0ff6
anikatasnin <jarintasnin09@gmail.com>
2025-06-09 21:01:33 +0600

    running server

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Penora Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is boiling on port: ${port}`);
+});
